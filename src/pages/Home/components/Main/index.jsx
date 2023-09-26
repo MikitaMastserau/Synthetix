20a0afcf5b218945f24ac70c1a8b5233d6010e11
MikitaMastserau <nik.redko@gmail.com>
2023-09-27 01:39:47 +0700

    init app, add header, main

---
+++ src/pages/Home/components/Main/index.jsx
@@ -0,0 +1,17 @@
+import circle from "static/images/circle.svg";
+
+import styles from "./styles.module.scss";
+
+export const Main = () => {
+   return (
+      <section className={styles.main}>
+         <div className={styles.container}>
+            <h1>The Derivаtives <span>Liquidity Protocol</span></h1>
+            <div className={styles.main__text}>
+               <p><strong>Synthetix</strong> is a new financial primitive enabling the creation of synthetic assets, offering unique derivatives and exposure to <span>real-world assets on the blockchain.</span></p>
+               <img src={circle} alt="" />
+            </div>
+         </div>
+      </section>
+   );
+};
